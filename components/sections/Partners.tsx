05adcef0c0e2d42f1912aca2324ccb081d5b6a2f
nonthasak laoluerat <nonthasak.l@Appels-iMac.local>
2023-04-08 21:34:51 +0700

    update: get rid of partner logo

--- components/sections/Partners.tsx
+++ components/sections/Partners.tsx
@@ -15,13 +15,14 @@ export default function PartnerSection() {
       <div>
         <div className="text-thuiwhite text-[35px]">Partners</div>
         <div className="flex justify-center">
-          <Image
+          <div className="text-thuiwhite text-3xl">To be announcement</div>
+          {/* <Image
             className="w-[300px]
             labtop:w-[500px]"
             src={bitkub}
             width={500}
             alt={"bitkub"}
-          />
+          /> */}
         </div>
       </div>
     </div>
