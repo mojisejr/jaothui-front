c7bf4ff2794edb700a1fd7bb07e0a01413a1accf
nonthasak laoluerat <nonthasak.l@Appels-iMac.local>
2023-06-10 23:02:03 +0700

    update: verified badge

--- components/sections/cert/Profile.tsx
+++ components/sections/cert/Profile.tsx
@@ -42,11 +42,10 @@ const Profile = () => {
             </span>
           </div>
           <div id="profile-email">email: {email ? email : "N/A"}</div>
-          <div
-            id="profile-verified"
-            className="text-thuidark bg-[#00ff22] pl-2 pr-2 rounded-md"
-          >
-            Verified
+          <div id="profile-verified" className="flex">
+            <div className="text-thuidark bg-[#00ff22] pl-2 pr-2 pt-1 pb-1 rounded-md">
+              Verified
+            </div>
           </div>
         </div>
       </div>
