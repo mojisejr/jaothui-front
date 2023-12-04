3129942a25f94a2078abf28e2e1d27ddaad34b7c
nonthasak laoluerat <nonthasak.l@Appels-iMac.local>
2023-12-04 20:44:57 +0700

    fix: metadata not found bug

--- pages/cert/[tokenId].tsx
+++ pages/cert/[tokenId].tsx
@@ -16,60 +16,64 @@ const CertDetail = () => {
   const { rewards } = useGetRewardByMicrochip(tokenId! as string);
   const { approvedBy } = useGetApprovalDataByMicrochip(tokenId! as string);
 
+  console.log(metadata);
+
   return (
     <Layout>
-      <Head>
-        <title key="title">{metadata[0].name}</title>
-        <meta
-          key="keywords"
-          name="keywords"
-          content={`Jaothui, JaothuiNFT, NFT, Pedigree, Kwaithai, Jaothui Official`}
-        />
-        <meta
-          key="description"
-          name="description"
-          content="ยกระดับควายไทย ยกระดับการอนุรักษ์ ส่งควายไทย ให้โด่งดังไปสู่ Global กับโปรเจกต์ ‘JAOTHUI NFT’"
-        />
-        <meta
-          key="og-title"
-          name="og:title"
-          property="og:title"
-          content={`${metadata[0].name} #${metadata[0].microchip}`}
-        />
-        <meta
-          key="og-description"
-          name="og:description"
-          property="og:description"
-          content="ยกระดับควายไทย ยกระดับการอนุรักษ์ ส่งควายไทย ให้โด่งดังไปสู่ Global กับโปรเจกต์ ‘JAOTHUI NFT’"
-        />
-        <meta
-          key="og-url"
-          name="og:url"
-          property="og:url"
-          content={`https://jaothui.com/cert/${metadata[0].microchip}`}
-        />
-        <meta
-          key="og-image"
-          name="og:image"
-          property="og:image"
-          content={`${metadata[0].image}`}
-        />
-        <meta
-          key="twitter-title"
-          name="twitter:title"
-          content={metadata[0].name}
-        />
-        <meta
-          key="twitter-description"
-          name="twitter:description"
-          content="ยกระดับควายไทย ยกระดับการอนุรักษ์ ส่งควายไทย ให้โด่งดังไปสู่ Global กับโปรเจกต์ ‘JAOTHUI NFT’"
-        />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:site" content="@jaothui" />
-        <meta name="twitter:image" content={`${metadata[0].image}`} />
+      {metadata.length <= 0 || metadata == undefined ? null : (
+        <Head>
+          <title key="title">{metadata[0].name}</title>
+          <meta
+            key="keywords"
+            name="keywords"
+            content={`Jaothui, JaothuiNFT, NFT, Pedigree, Kwaithai, Jaothui Official`}
+          />
+          <meta
+            key="description"
+            name="description"
+            content="ยกระดับควายไทย ยกระดับการอนุรักษ์ ส่งควายไทย ให้โด่งดังไปสู่ Global กับโปรเจกต์ ‘JAOTHUI NFT’"
+          />
+          <meta
+            key="og-title"
+            name="og:title"
+            property="og:title"
+            content={`${metadata[0].name} #${metadata[0].microchip}`}
+          />
+          <meta
+            key="og-description"
+            name="og:description"
+            property="og:description"
+            content="ยกระดับควายไทย ยกระดับการอนุรักษ์ ส่งควายไทย ให้โด่งดังไปสู่ Global กับโปรเจกต์ ‘JAOTHUI NFT’"
+          />
+          <meta
+            key="og-url"
+            name="og:url"
+            property="og:url"
+            content={`https://jaothui.com/cert/${metadata[0].microchip}`}
+          />
+          <meta
+            key="og-image"
+            name="og:image"
+            property="og:image"
+            content={`${metadata[0].image}`}
+          />
+          <meta
+            key="twitter-title"
+            name="twitter:title"
+            content={metadata[0].name}
+          />
+          <meta
+            key="twitter-description"
+            name="twitter:description"
+            content="ยกระดับควายไทย ยกระดับการอนุรักษ์ ส่งควายไทย ให้โด่งดังไปสู่ Global กับโปรเจกต์ ‘JAOTHUI NFT’"
+          />
+          <meta name="twitter:card" content="summary_large_image" />
+          <meta name="twitter:site" content="@jaothui" />
+          <meta name="twitter:image" content={`${metadata[0].image}`} />
 
-        <link rel="canonical" href="https://jaothui.com/" />
-      </Head>
+          <link rel="canonical" href="https://jaothui.com/" />
+        </Head>
+      )}
       {metadata == undefined || metadata == null || metadata.length <= 0 ? (
         <div className="min-h-screen flex justify-center">
           <Loading size="lg" />
