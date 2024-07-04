ed0d0b3d72df436c3fff169edd284911f49ddc52
Nonthasak Laoluerat <nonthasak.l@gmail.com>
2024-07-04 22:17:19 +0700

    upd: menu icons, list buffalo owner by name

--- components/Profile/ProfileMenuList.tsx
+++ components/Profile/ProfileMenuList.tsx
@@ -1,46 +1,115 @@
 import React from "react";
 import Link from "next/link";
 import Image from "next/image";
-import { FaLine } from "react-icons/fa";
-import { GiFarmTractor } from "react-icons/gi";
-import { RiVipDiamondFill } from "react-icons/ri";
 
 const ProfileMenuList = () => {
   return (
-    <div className="grid grid-cols-4 gap-4 w-full p-2 rounded-xl">
+    <div className="grid grid-cols-3 gap-4 w-full p-2 rounded-xl">
       <Link
         href="https://line.me/R/ti/p/@551lwpun"
-        className="grid grid-cols-1 place-items-center bg-neutral p-2 rounded-xl bg-opacity-30"
+        className="grid grid-cols-1 place-items-center bg-opacity-30 gap-1"
       >
-        <FaLine size={28} />
-        <span className="text-xs">Event</span>
+        <figure className="w-24">
+          <Image
+            src="/images/icons/EVENT.png"
+            height={1000}
+            width={1000}
+            alt="jaothui event"
+          />
+        </figure>
+        <div className="flex flex-col items-center">
+          <span className="text-xs">Event</span>
+          <span className="text-[9px]">Jaothui Event</span>
+        </div>
       </Link>
       <Link
         href="https://kwaithai.com/"
-        className="grid grid-cols-1 place-items-center bg-neutral p-2 rounded-xl bg-opacity-30"
+        className="grid grid-cols-1 place-items-center gap-1 rounded-xl bg-opacity-30"
       >
-        {/* <FaLine size={28} /> */}
-        <Image
-          src="/images/logo.png"
-          height={28}
-          width={28}
-          alt="kwaithai.com"
-        />
-        <span className="text-xs">Cert</span>
+        <figure className="w-24">
+          <Image
+            src="/images/icons/KWAITHAI.png"
+            height={1000}
+            width={1000}
+            alt="kwaithai.com"
+          />
+        </figure>
+        <div className="flex flex-col items-center">
+          <span className="text-xs">Cert</span>
+          <span className="text-[9px]">Certificate</span>
+        </div>
       </Link>
       <Link
-        href="/cert/profile/privilege"
-        className="grid grid-cols-1 place-items-center bg-neutral p-2 rounded-xl bg-opacity-30"
+        href="https://www.facebook.com/jaothui"
+        target="_blank"
+        className="grid grid-cols-1 place-items-center gap-1 rounded-xl bg-opacity-30"
       >
-        <RiVipDiamondFill size={28} />
-        <span className="text-xs">Privilege</span>
+        <figure className="w-24">
+          <Image
+            src="/images/icons/FACEBOOK.png"
+            height={1000}
+            width={1000}
+            alt="facebook"
+          />
+        </figure>
+        <div className="flex flex-col items-center">
+          <span className="text-xs">Facebook</span>
+          <span className="text-[9px]">Fanpage</span>
+        </div>
       </Link>
+
       <Link
         href="/cert/profile/myfarm"
-        className="grid grid-cols-1 place-items-center bg-neutral p-2 rounded-xl bg-opacity-30"
+        className="grid grid-cols-1 place-items-center gap-1 rounded-xl bg-opacity-30"
       >
-        <GiFarmTractor size={28} />
-        <span className="text-xs">My Farm</span>
+        <figure className="w-24">
+          <Image
+            src="/images/icons/MY-FARM.png"
+            height={1000}
+            width={1000}
+            alt="my farm"
+          />
+        </figure>
+        <div className="flex flex-col items-center">
+          <span className="text-xs">My Farm</span>
+          <span className="text-[9px]">Farm Management</span>
+        </div>
+      </Link>
+
+      <Link
+        href="/cert/profile/privilege"
+        className="grid grid-cols-1 place-items-center gap-1 rounded-xl bg-opacity-30"
+      >
+        <figure className="w-24">
+          <Image
+            src="/images/icons/PRIVILEGE.png"
+            height={1000}
+            width={1000}
+            alt="privilege"
+          />
+        </figure>
+        <div className="flex flex-col items-center">
+          <span className="text-xs">Previlege</span>
+          <span className="text-[9px]">Reward & Redeem</span>
+        </div>
+      </Link>
+
+      <Link
+        href="#"
+        className="grid grid-cols-1 place-items-center gap-1 rounded-xl bg-opacity-30"
+      >
+        <figure className="w-24">
+          <Image
+            src="/images/icons/SHOP.png"
+            height={1000}
+            width={1000}
+            alt="facebook"
+          />
+        </figure>
+        <div className="flex flex-col items-center">
+          <span className="text-xs">SHOP</span>
+          <span className="text-[9px]">coming soon</span>
+        </div>
       </Link>
     </div>
   );
--- server/services/kwaithai.service.ts
+++ server/services/kwaithai.service.ts
@@ -7,19 +7,25 @@ export const getMemberData = async (wallet: string) => {
   try {
     const user = await prisma.user.findUnique({
       where: { wallet },
-      include: {
-        Certificate: { select: { microchip: true } },
-      },
+      // where: { wallet: "0x0f454775C7833A624C7b981cf9671806Fc880E30" },
     });
+
+    const certificates = user
+      ? await prisma.certificate.findMany({ where: { ownerName: user.name! } })
+      : [];
+
+    console.log("ownerName: ", user?.name);
+
+    console.log("cert: ", certificates);
 
     const avatar = getAvatarUrl(user?.avatar!);
 
     const allMeta = await getAllMetadata();
 
     const userBuffaloMata =
-      user && user?.Certificate.length <= 0
+      user && certificates.length <= 0
         ? []
-        : user?.Certificate.map((cert) => {
+        : certificates.map((cert) => {
             const found = allMeta?.find(
               (meta) => meta.microchip == cert.microchip
             );
