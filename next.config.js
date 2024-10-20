397a2c181be18206072a3f9ba9a1125ef30e5ef2
Nonthasak Laoluerat <nonthasak.l@gmail.com>
2024-10-20 10:50:42 +0700

    upd: update image-caching

--- next.config.js
+++ next.config.js
@@ -1,11 +1,28 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: "/storage/v1/object/public/slipstorage/buffalo/:path*",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=604800",
+          },
+        ],
+      },
+    ];
+  },
   images: {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "**",
+        hostname: "https://wtnqjxerhmdnqszkhbvs.supabase.co",
+      },
+      {
+        protocol: "https",
+        hostname: "flagcdn.com",
       },
     ],
   },
